eab7898be489213b231c9a7d874a12b550079d02
Kidus Kinfe <53956331+KidusY@users.noreply.github.com>
2020-02-22 09:36:36 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+// on scroll change css properties
+$(window).scroll(function () {
+    let position = $('nav').offset().top;
+    let portfolio = $('.portfolio h1').offset().top;
+    let scrolled = window.pageYOffset;
+    let rate = scrolled * 2;
+    //parallex 
+    console.log(rate);
+    $('landingPage img').css(`transform`, `translateY(${rate}px)`);
+
+
+
+    if (position >= portfolio) {
+        console.log("asdfsadf");
+        $('.portfolio h1').fadeIn(3000, () => $('.portfolio h1').show());
+    }
+
+
+    //changes the nav bar css property
+    if (position > 100) {
+        $('nav ul').css('background-color', "#1f1f1f");
+        $('nav ul').css('padding', "20px");
+
+    } else {
+        $('nav ul').css('background', "none");
+        $('nav ul').css('padding', "50px 10px");
+    }
+    if (position > 500) {
+        $('.gotoTop').show();
+    } else {
+        $('.gotoTop').hide();
+    }
+});
+
+
+//scroll on click
+function scrollAnimation() {
+    $('.menuList a, header nav ul a, .btn-seeMore , .gotoTop-btn').click(function (e) {
+        e.preventDefault();
+        let hash = this.hash;
+        //scrolls to the location of the sections 
+        $('html,body').animate({
+            scrollTop: $(hash).offset().top
+        }, 700)
+        //hide animation for the menu list
+        $('.menuList').fadeOut(() => {
+            $('.menuList').hide();
+        });
+
+
+    });
+
+}
+
+
+
+
+function main() {
+    //mobile hamburger icon toggle
+    $('.menu-btn').click(() => {
+
+            $('.menuList').toggle("slow");
+
+        }
+
+    )
+    $('.btn-seeMore').hide();
+    //onload animate landing page
+    $('.landingPage img').fadeIn(3000, () => $('.landingPage img, .btn-seeMore').show());
+    $('.btn-seeMore').fadeIn(4000, () => $('.btn-seeMore').show());
+
+    scrollAnimation();
+}
+
+$(main);
